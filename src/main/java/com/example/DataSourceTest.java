1865b44a23b43850665bf22f6e680d0079b5f768
CoramDeo03 <kajonman0616@gmail.com>
2023-12-16 04:09:23 +0900

    first commit

---
+++ src/main/java/com/example/DataSourceTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.junit.Test;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSourceTest {
+    @Test
+    public  void testConnection(){
+        ApplicationContext ctx=
+                new FileSystemXmlApplicationContext(
+                        "file:src/main/webapp/WEB-INF/applicationContext.xml"
+                );
+        DataSource ds = (DataSource) ctx.getBean("dataSource");
+        try{
+            Connection con= ds.getConnection();
+            System.out.println("fuck");
+        }catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("hahahah");
+        }
+    }
+
+}
